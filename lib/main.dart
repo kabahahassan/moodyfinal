082336b5cda1c385fffe5b50ad6d75043e4322af
hassan <kabahahassan@gmail.com>
2020-05-14 20:48:07 +0300

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'round_button.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('MOODY STYLE'),
           backgroundColor: Colors.black,
         ),
-        backgroundColor: Colors.white,
+        //backgroundColor: Colors.white,
         body: SafeArea(
           child: Center(
             child: Column(
@@ -24,25 +26,23 @@ class MyApp extends StatelessWidget {
                 CircleAvatar(
                   backgroundColor: Colors.white,
                   backgroundImage: AssetImage('images/mobile_logo_white.png'),
-                  radius: 50.0,
+                  radius: 100.0,
                 ),
                 Text(
                   '#MoodyStyle',
                   style: TextStyle(
                     fontSize: 20.0,
-                    color: Colors.black45,
+                    color: Colors.white70,
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.5,
-                    fontFamily: 'Source Sans Pro',
                   ),
                 ),
                 Text(
                   'Smiles Are Always In Fashion',
                   style: TextStyle(
-                    fontSize: 28.0,
-                    color: Colors.black,
+                    fontSize: 24.0,
+                    color: Colors.white,
                     fontWeight: FontWeight.bold,
-                    fontFamily: 'Pacifico',
                   ),
                 ),
                 SizedBox(
@@ -52,47 +52,18 @@ class MyApp extends StatelessWidget {
                     color: Colors.black45,
                   ),
                 ),
-                InkWell(
-                  onTap: () => launch("sms:0502801437"),
-                  child: Card(
-                    color: Colors.black,
-                    margin:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.phone,
-                        color: Colors.white70,
-                      ),
-                      title: Text(
-                        'צור קשר',
-                        style: TextStyle(
-                          fontFamily: 'Source Sans Pro',
-                          color: Colors.white70,
-                        ),
-                      ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    RoundIconButton(
+                      icon: Icons.phone,
+                      onPressed: () => launch("sms:0502801437"),
                     ),
-                  ),
-                ),
-                InkWell(
-                  onTap: _launchURL,
-                  child: Card(
-                    color: Colors.black,
-                    margin:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.store,
-                        color: Colors.white70,
-                      ),
-                      title: Text(
-                        'בקר בחנות שלנו',
-                        style: TextStyle(
-                          fontFamily: 'Source Sans Pro',
-                          color: Colors.white70,
-                        ),
-                      ),
+                    RoundIconButton(
+                      icon: Icons.store,
+                      onPressed: _launchURL,
                     ),
-                  ),
+                  ],
                 ),
               ],
             ),
